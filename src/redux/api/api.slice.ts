7bb136a1d25e894b9662e2cf2eab44c76ab4193b
Nimesh Shakya <57608111+Nimesh-bot@users.noreply.github.com>
2023-07-11 10:36:39 +0545

    feat: signUp complete

--- src/redux/api/api.slice.ts
+++ src/redux/api/api.slice.ts
@@ -1,25 +1,29 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import Toast from "react-native-toast-message";
-import { authorize, unauthorize } from "../features/auth/auth.slice";
-import { RootState } from "../store";
+import { unauthorize } from "../features/auth/auth.slice";
 
 const baseQuery = fetchBaseQuery({
-    // baseUrl: 'https://prachaya-test-back.onrender.com/',
-    baseUrl: 'https://3d60-202-51-76-77.in.ngrok.io',
-    prepareHeaders: (headers, { getState }) => {
-      // const token = (getState() as RootState).auth.access;
-      // if (token) {
-      //   headers.set("authorization", `${token}`);
-      // }
-      return headers;
-    },
+  // baseUrl: 'https://prachaya-test-back.onrender.com/',
+  // baseUrl: 'https://3d60-202-51-76-77.in.ngrok.io',
+  baseUrl: "http://192.168.101.3:8000",
+  prepareHeaders: (headers, { getState }) => {
+    // const token = (getState() as RootState).auth.access;
+    // if (token) {
+    //   headers.set("authorization", `${token}`);
+    // }
+    return headers;
+  },
 });
 
-export const baseQueryWithoutErrorHandling = async ( args: any, api: any, extraOptions: any ) => {
-  console.log("Hitting now")
+export const baseQueryWithoutErrorHandling = async (
+  args: any,
+  api: any,
+  extraOptions: any
+) => {
+  console.log("Hitting now");
   const result = await baseQuery(args, api, extraOptions);
-  console.log("API result:", result)
-  
+  console.log("API result:", JSON.stringify(result));
+
   if (result.data) {
     if (args.method !== "GET" && typeof args !== "string")
       console.log("API success:", args.typePrefix);
@@ -35,31 +39,35 @@ export const baseQueryWithoutErrorHandling = async ( args: any, api: any, extraO
   return result;
 };
 
-export const baseQueryWithErrorHandling = async ( args: any, api: any, extraOptions: any ) => {
+export const baseQueryWithErrorHandling = async (
+  args: any,
+  api: any,
+  extraOptions: any
+) => {
   const result = await baseQuery(args, api, extraOptions);
 
   if (result.error) {
     if (result.error.status === 400) {
-        console.error("API validation error:", (result.error.data as any).error);
-        Toast.show({
-            type: "error",
-            text1: (result.error.data as any).error,
-        })
+      console.error("API validation error:", (result.error.data as any).error);
+      Toast.show({
+        type: "error",
+        text1: (result.error.data as any).error,
+      });
     } else if (result.error.status === 401) {
-        api.dispatch(unauthorize());
-        Toast.show({
-            type: "error",
-            text1: "Unauthorized. Please login again.",  
-        })
+      api.dispatch(unauthorize());
+      Toast.show({
+        type: "error",
+        text1: "Unauthorized. Please login again.",
+      });
     } else if (result.error.data) {
-        console.error("API error:", result.error.data);
-        Toast.show({
-            type: "error",
-            text1: (result.error.data as any).error,
-        })
+      console.error("API error:", result.error.data);
+      Toast.show({
+        type: "error",
+        text1: (result.error.data as any).error,
+      });
     }
   }
-  
+
   return result;
 };
 
--- src/screens/AuthScreen/Register.tsx
+++ src/screens/AuthScreen/Register.tsx
@@ -1,150 +1,165 @@
-import { View, Text, Pressable, KeyboardAvoidingView, Platform, ScrollView } from 'react-native'
-import React, { SetStateAction, useState } from 'react'
+import React, { useState } from "react";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 
-import { RightArrowIcon } from '../../../assets/icons/svg-icons'
-import { InputFieldWithPrefix } from '../../components/InputFields'
-import { PrimaryButton } from '../../components/Buttons'
+import { RightArrowIcon } from "../../../assets/icons/svg-icons";
 
-import RegisterInfo from './components/RegisterInfo'
-import RegisterPhone from './components/RegisterPhone'
-import { Dispatch } from '@reduxjs/toolkit'
-import OPTVerification from './components/OPTVerification'
+import OPTVerification from "./components/OPTVerification";
+import RegisterInfo from "./components/RegisterInfo";
+import RegisterPhone from "./components/RegisterPhone";
 
-import { useVerifyOtpMutation } from '../../redux/features/auth/authApi.slice';
-import Toast from 'react-native-toast-message'
-import LoadingIndicator from '../../components/LoadingIndicator'
+import Toast from "react-native-toast-message";
+import LoadingIndicator from "../../components/LoadingIndicator";
+import { useVerifyOtpMutation } from "../../redux/features/auth/authApi.slice";
 
 export interface IRegisterInfo {
-  firstname: string
-  middlename: string
-  lastname: string
-  email: string
-  dob: Date
-  mobile_number: string
-  code: string
-  step: number
+  firstname: string;
+  middlename: string;
+  lastname: string;
+  email: string;
+  dob: Date;
+  mobile_number: string;
+  code: string;
+  step: number;
 }
 export interface RegisterProps {
-  registerInfo: IRegisterInfo
-  setRegisterInfo: Function
+  registerInfo: IRegisterInfo;
+  setRegisterInfo: Function;
 }
 
 const Register = ({ navigation }: any) => {
   const [registerInfo, setRegisterInfo] = useState<IRegisterInfo>({
-    firstname: '',
-    middlename: '',
-    lastname: '',
-    email: '',
+    firstname: "",
+    middlename: "",
+    lastname: "",
+    email: "",
     dob: new Date(),
-    mobile_number: '',
-    code: '',
+    mobile_number: "",
+    code: "",
     step: 1,
-  })
+  });
 
-  const [ verifyOTP, { data, isLoading } ] = useVerifyOtpMutation()
-  const [error, setError] = useState('')
-  
+  const [verifyOTP, { data, isLoading }] = useVerifyOtpMutation();
+  const [error, setError] = useState("");
 
   const handleOTPVerification = (input_code: string) => {
-    console.log('here', )
-    setError('')
-    if(input_code.length !== 6) {
-      setError('Please enter the 6 digit OTP sent to your phone number')
-    } else if(registerInfo.code !== input_code) {
-      console.log(registerInfo.code, input_code)
-      console.log('here')
-      setError('Invalid OTP')
+    console.log("here");
+    setError("");
+    if (input_code.length !== 6) {
+      setError("Please enter the 6 digit OTP sent to your phone number");
+    } else if (registerInfo.code !== input_code) {
+      console.log(registerInfo.code, input_code);
+      console.log("here");
+      setError("Invalid OTP");
     } else {
       const payload = {
         mobile_number: registerInfo.mobile_number,
         otp: input_code,
-        type: 'signup'
-      }
-      verifyOTP(payload).unwrap().then((res) => {
-        Toast.show({
-          type: 'success',
-          text1: 'Verified',
-          text2: 'Phone number verified successfully. Redirecting to final step in 3 seconds',
+        type: "signup",
+      };
+      verifyOTP(payload)
+        .unwrap()
+        .then((res) => {
+          Toast.show({
+            type: "success",
+            text1: "Verified",
+            text2:
+              "Phone number verified successfully. Redirecting to final step in 3 seconds",
+          });
+          setTimeout(() => {
+            setRegisterInfo({
+              ...registerInfo,
+              step: 3,
+            });
+          }, 3000);
         })
-        setTimeout(() => {
-          setRegisterInfo({
-            ...registerInfo,
-            step: 3
-          })
-        }, 3000)  
-      }).catch((err) => {
-        Toast.show({
-          type: 'error',
-          text1: 'Error',
-          text2: err.data.message || 'Something went wrong. Please try again later',
-        })
-      })
+        .catch((err) => {
+          Toast.show({
+            type: "error",
+            text1: "Error",
+            text2:
+              err.data.message ||
+              "Something went wrong. Please try again later",
+          });
+        });
     }
-  }
-
+  };
 
   return (
-    <KeyboardAvoidingView 
+    <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
-      className='p-xl w-full flex-col justify-between flex-1'
+      className="p-xl w-full flex-col justify-between flex-1"
     >
       <ScrollView>
-        <Pressable className='flex-row items-center' onPress={() => {
-          navigation.navigate('Login')
-        }}>
-            <View className='rotate-180 max-w-fit'>
-                <RightArrowIcon size={21} />
-            </View>
-            <Text className='ml-default'>Back to Login</Text>
+        <Pressable
+          className="flex-row items-center"
+          onPress={() => {
+            navigation.navigate("Login");
+          }}
+        >
+          <View className="rotate-180 max-w-fit">
+            <RightArrowIcon size={21} />
+          </View>
+          <Text className="ml-default">Back to Login</Text>
         </Pressable>
 
-        <View className='mt-2xl flex-row items-center'>
-          <Text className='text-3xl text-dark'>Step</Text>
-          <Text className='text-3xl font-bold text-blue mx-xs'>{registerInfo.step}</Text>
-          <Text className='text-3xl text-dark'>of 3</Text>
+        <View className="mt-2xl flex-row items-center">
+          <Text className="text-3xl text-dark">Step</Text>
+          <Text className="text-3xl font-bold text-blue mx-xs">
+            {registerInfo.step}
+          </Text>
+          <Text className="text-3xl text-dark">of 3</Text>
         </View>
 
-        <View className='mt-2xl'>
-            <Text className='text-xl font-bold text-dark'>Get Started</Text>
-            {
-              registerInfo.step === 1 ?
-              <Text className='text-dark opacity-80'>We require you to verify your phone and provide your information to continue</Text>
-              :
-              <Text className='text-dark opacity-80'>Join the helpful community and contribute in creating a helpful environment.</Text>
-            }
+        <View className="mt-2xl">
+          <Text className="text-xl font-bold text-dark">Get Started</Text>
+          {registerInfo.step === 1 ? (
+            <Text className="text-dark opacity-80">
+              We require you to verify your phone and provide your information
+              to continue
+            </Text>
+          ) : (
+            <Text className="text-dark opacity-80">
+              Join the helpful community and contribute in creating a helpful
+              environment.
+            </Text>
+          )}
         </View>
 
-        <View className='mt-2xl'>
-          {
-            registerInfo.step === 1 ?
-            <RegisterPhone 
+        <View className="mt-2xl">
+          {registerInfo.step === 1 ? (
+            <RegisterPhone
               registerInfo={registerInfo}
               setRegisterInfo={setRegisterInfo}
             />
-            :
-            registerInfo.step === 2 ?
+          ) : registerInfo.step === 2 ? (
             <OPTVerification
               phone={registerInfo.mobile_number}
               error={error}
               handleOTPVerification={handleOTPVerification}
             />
-            :
-            <RegisterInfo 
+          ) : (
+            <RegisterInfo
               registerInfo={registerInfo}
               setRegisterInfo={setRegisterInfo}
             />
-          }
+          )}
         </View>
       </ScrollView>
-      {
-        isLoading &&
-        <LoadingIndicator 
-          text='Verifying OTP' 
-          subText='Please wait while we verify the OTP'
+      {isLoading && (
+        <LoadingIndicator
+          text="Verifying OTP"
+          subText="Please wait while we verify the OTP"
         />
-      }
+      )}
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
